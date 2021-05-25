b60fd8d88b879dc773eeb00acaf8b8ab9d1aebdd
sandorjozsef <sjozsef2000@gmail.com>
2021-05-25 18:31:59 +0200

    tests

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -21,25 +21,171 @@ public class GT4500Test {
   }
 
   @Test
-  public void fireTorpedo_Single_Success(){
+  public void fireTorpedo_Single_Success_State_1(){
     // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+    
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_State_2(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, result);
+    
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_Interaction(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(1)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Fail_State(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Fail_Interaction(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    verify(mockTS1, times(0)).fire(1);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_whenPrimaryFirst_State_1(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+    // Assert
+    assertEquals(true, result2);
+  }
+
+  @Test
+  public void fireTorpedo_Single_Success_whenPrimaryFirst_State_2(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean result1 = ship.fireTorpedo(FiringMode.SINGLE);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+    boolean result2 = ship.fireTorpedo(FiringMode.SINGLE);
+    // Assert
+    assertEquals(true, result2);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Fail(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(false);
+   
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Fail_2(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(false);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(true);
+   
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireTorpedo_All_Fail_3(){
+    // Arrange
+    when(mockTS1.isEmpty()).thenReturn(false);
+    when(mockTS1.fire(1)).thenReturn(true);
+    when(mockTS2.isEmpty()).thenReturn(false);
+    when(mockTS2.fire(1)).thenReturn(false);
+   
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireLaser_Fail(){
+    // Arrange
+   
+   
+    // Act
+    boolean result = ship.fireLaser(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
   }
 
 }
